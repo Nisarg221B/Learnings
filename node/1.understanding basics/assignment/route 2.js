f851696018c50a8494520438f802986d14050376
Nisarg221B <nisarg.b.patel59@gmail.com>
2023-09-12 20:07:49 +0530

    commiting node modules

---
+++ node/1.understanding basics/assignment/route 2.js
@@ -0,0 +1,58 @@
+const usersList = ['nisarg', 'payal'];
+
+function requestListner(req, res) {
+    const url = req.url;
+    const method = req.method;
+    // console.log(url," ",method," ",req.headers);
+    if (url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body>');
+        res.write('<h1>Greetings! from Nisarg</h1>');
+        res.write('<h3>Register a User</h3>');
+        res.write('<form action="/users" method="POST"><input type="text" name="user">');
+        res.write('<button type="submit">Register</button>');
+        res.write('</form>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === "/users" && method === "POST"){
+        let body = [];
+        req.on('data',(chunk)=>{
+            body.push(chunk);
+        });
+
+        return req.on('end',()=>{
+            let usr = Buffer.concat(body).toString().split('=')[1];
+            console.log(usr);
+            usersList.push(usr);
+            res.statusCode = 302;
+            res.setHeader('Location', '/users');
+            res.setHeader('Method','GET');
+            res.end();
+        });
+    }
+    if (url === "/users") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html>');
+        res.write('<head><title>users list page</title></head>');
+        res.write('<body><h1>Registered Users</h1>');
+        for (usr in usersList) {
+            res.write(`<ul><li><b>${usersList[usr]}</b></li></ul>`);
+        }
+        res.write('<form method="POST" action="/"><button type="submit">back</button></form>')
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html>');
+    res.write('<head><title>Page not found</title></head>');
+    res.write('<body><h3>Page not found</h3>');
+    res.write('</body>');
+    res.write('</html>');
+}
+
+module.exports = requestListner;
---
+++ node/1.understanding basics/readme 2.md
@@ -0,0 +1,23 @@
+### Basics 
+
+- Node basics
+    - Event loop
+    - callback architecture
+
+- Basic Server
+    - 'http' core node module 
+    - creating a server
+    - writing the eventListner function for requests
+    - req.url , req.method , req.headers , req.on (for setting event listners for specifics)
+    - based on request url and method , sending the respective web page back as response
+    - Streams and buffers for receiving data
+
+- Module management 
+    - Exporting and Importing local modules 
+
+- Types of Errors
+    - Syntax Errors 
+    - Runtime Errors
+    - Logical Errors
+        - Using VS-code Debugger
+        - using nodemon for autorestarting debugger (see .vscode/launch.json file)
