ce98c6616e564166a0a7cc52a058b4267e35d47c
Nisarg221B <nisarg.b.patel59@gmail.com>
2023-09-30 07:56:49 +0530

    feat: added versioning to the api
    versioned all the api routes by seperating the version 1 routes in
    /server/src/routes/versions/v1.api.js which exports a router containing
    v1 routes. i.e. /v1/launches/.. type of endpoints. this makes our
    api future proof as if we had any breaking changes in the future for
    our api we could launch a version 2 and still the aplications using
    our api won't break as they will be running on version 1 so they have
    time to migrate to version 2 if need be.
    
    signed-off-by: Nisarg Patel(patelnjisarg118@gmail.com).

--- node/8. NASA-PROJECT-MongoDB/server/src/app.js
+++ node/8. NASA-PROJECT-MongoDB/server/src/app.js
@@ -1,12 +1,9 @@
 const path = require('path');
-
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const planetsRouter = require('./routes/planets/planets.router');
-const launchsRouter = require('./routes/launches/launches.router');
-
+const v1router = require('./routes/versions/v1.api'); // for supporting multiple versions
 const app = express(); // just a fancy listener function
 
 // **************** Logger - morgan ****************** 
@@ -19,8 +16,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // **************** Routes ****************** 
-app.use('/launches',launchsRouter); // lauches routes
-app.use('/planets',planetsRouter); // planet routes
+app.use('/v1',v1router);
+// app.use('/v2',v2router);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
