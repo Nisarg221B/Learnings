4cb5a0f45625ccc548772d90a9209554ed4c4a39
Nisarg221B <nisarg.b.patel59@gmail.com>
2023-10-04 18:28:31 +0530

    feat(security) - added helmet to the server for added security

---
+++ node/9. Security/b. Helmet-Addon/server.js
@@ -0,0 +1,27 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const helmet = require('helmet');
+
+const PORT = 3000;
+
+const app = express();
+
+//app.use(helmet()); // sets all the default headers
+
+app.get('/sercret', (req, res) => {
+    return res.send('Your personal secret value is 42!');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+});
